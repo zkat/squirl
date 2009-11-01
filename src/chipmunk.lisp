a5b12e53ae6eaa5a0dcc299e8c6c8b1b3b763354
Kat Marchán <kzm@sykosomatic.org>
2009-10-31 21:39:09 -0500

    Added chipmunk.lisp

---
+++ src/chipmunk.lisp
@@ -0,0 +1,76 @@
+(in-package :squirl)
+
+(declaim (inline min max clamp))
+(defun max (a b)
+  (if (> a b) a b))
+
+(defun min (a b)
+  (if (< a b) a b))
+
+(defun clamp (n min max)
+  (min (max n min) max))
+
+(defun init-collision-funcs ()
+  ;; todo
+  )
+
+(defun init ()
+  (init-collision-funcs))
+
+(defun moment-for-circle (m inner-diameter outer-diameter offset)
+  "Calculate the moment of inertia for a circle. r1 and r2 are the inner and outer diameters.
+A solid circle has an inner diameter of 0."
+  ;; c version: return (1.0f/2.0f)*m*(inner*inner + outer*outer) + m*cpvdot(offset, offset);
+  (+ 
+   (* m 1/2 (+ (expt inner-diameter 2)
+               (expt outer-diameter 2)))
+   (* m (vector-dot offset offset))))
+
+(defun moment-for-segment (m a b)
+  "Calculate the moment of inertia for a line segment. Beveling radius not supported."
+  ;; C version:
+  ;; cpFloat length = cpvlength(cpvsub(b, a));
+  ;; cpVect offset = cpvmult(cpvadd(a, b), 1.0f/2.0f);
+  ;; return m*length*length/12.0f + m*cpvdot(offset, offset);
+  (let ((length (length (vector-subtract b a)))
+        (offset (vector-multiply (vector-add a b) 1/2)))
+    (+
+     (* m length (/ length 12))
+     (* m vector-dot offset offset))))
+
+(defun moment-for-poly (m num-verts verts offset)
+  "Calculate the moment of inertia for a solid palygon shape."
+  ;; C version:
+  ;; cpVect *tVerts = (cpVect *)calloc(numVerts, sizeof(cpVect));
+  ;; for(int i=0; i<numVerts; i++)
+  ;; 	tVerts[i] = cpvadd(verts[i], offset);
+	
+  ;; cpFloat sum1 = 0.0f;
+  ;; cpFloat sum2 = 0.0f;
+  ;; for(int i=0; i<numVerts; i++){
+  ;; 	cpVect v1 = tVerts[i];
+  ;; 	cpVect v2 = tVerts[(i+1)%numVerts];
+		
+  ;; 	cpFloat a = cpvcross(v2, v1);
+  ;; 	cpFloat b = cpvdot(v1, v1) + cpvdot(v1, v2) + cpvdot(v2, v2);
+		
+  ;; 	sum1 += a*b;
+  ;; 	sum2 += a;
+  ;; }
+	
+  ;; free(tVerts);
+  ;; return (m*sum1)/(6.0f*sum2);
+  (let ((t-verts (make-array num-verts)))
+    (loop for i below num-verts
+       do (setf (svref t-verts i) (vector-add (elt verts i) offset)))
+    (loop with sum1 = 0 with sum2 = 0
+       for i below num-verts
+       for v1 in t-verts
+       for v2 = (elt t-verts (mod (1+ i) num-verts))
+       for a = (vector-cross v1 v2)
+       for b = (+ (vector-dot v1 v1) (vector-dot v1 v2) (vector-dot v2 v2))
+       do (incf sum1 (* a b))
+         (incf sum2 a)
+       finally (return (/ (* m sum1)
+                          (* 6 sum2))))))
+
