a053c19a3c97606eca859e007da61c632d87dfa1
Ralith <ralith@arbor.localdomain>
2009-11-01 00:18:21 -0700

    Shortened the names of most common vector math functions.

--- src/vect.lisp
+++ src/vect.lisp
@@ -24,7 +24,7 @@
   (atan (vector-y vector)
         (vector-x vector)))
 
-(defun vector-add (v1 v2)
+(defun vec+ (v1 v2)
   (vector (+ (vector-x v1)
              (vector-x v2))
           (+ (vector-y v1)
@@ -34,24 +34,24 @@
   (vector (- (vector-x vector))
           (- (vector-y vector))))
 
-(defun vector-subtract (v1 v2)
+(defun vec- (v1 v2)
   (vector (- (vector-x v1)
              (vector-x v2))
           (- (vector-y v1)
              (vector-y v2))))
 
-(defun vector-multiply (vector s)
+(defun vec* (vector s)
   (vector (* (vector-x vector) s)
           (* (vector-y vector) s)))
 
-(defun vector-dot (v1 v2)
+(defun vec. (v1 v2)
   (+
    (* (vector-x v1)
       (vector-x v2))
    (* (vector-y v1)
       (vector-y v2))))
 
-(defun vector-cross (v1 v2)
+(defun vecx (v1 v2)
   (-
    (* (vector-x v1)
       (vector-y v2))
@@ -67,8 +67,8 @@
           (- (vector-x vector))))
 
 (defun vector-project (v1 v2)
-  (vector-multiply v2 (/ (vector-dot v1 v2)
-                         (vector-dot v2 v2))))
+  (vec* v2 (/ (vec. v1 v2)
+              (vec. v2 v2))))
 
 (defun vector-rotate (v1 v2)
   (vector (- (* (vector-x v1)
@@ -91,15 +91,15 @@
                 (vector-y v2)))))
 
 (defun vector-length-sq (vector)
-  (vector-dot vector vector))
+  (vec. vector vector))
 
 (defun vector-lerp (v1 v2 t)
-  (vector-add
-   (vector-multiply v1 (- 1 t))
-   (vector-multiply v2 t)))
+  (vec+
+   (vec* v1 (- 1 t))
+   (vec* v2 t)))
 
 (defun vector-normalize (vector)
-  (vector-multiply vector (/ 1 (vector-length vector))))
+  (vec* vector (/ 1 (vector-length vector))))
 
 (defun vector-normalize-safe (vector)
   (if (and (= 0 (vector-x vector))
@@ -108,16 +108,16 @@
       (vector-nermalize vector)))
 
 (defun vector-clamp (vector len)
-  (if (> (vector-dot vector vector)
+  (if (> (vec. vector vector)
          (* len len))
-      (vector-multiply (vector-normalize vector) len)
+      (vec* (vector-normalize vector) len)
       vector))
 
 (defun vector-dist (v1 v2)
-  (vector-length (vector-subtract v1 v2)))
+  (vector-length (vec- v1 v2)))
 
 (defun vector-dist-sq (v1 v2)
-  (vector-length-sq (vector-subtract v1 v2)))
+  (vector-length-sq (vec- v1 v2)))
 
 (defun vector-near (v1 v2 dist)
   (< (vector-dist-sq v1 v2)
