b24fe07787919a48cd66f67be0759576390339de
Ralith <ralith@arbor.localdomain>
2009-11-01 01:10:28 -0700

    Defined the contact structure.

--- src/arbiter.lisp
+++ src/arbiter.lisp
@@ -7,3 +7,17 @@
   "Determines how fast penetrations resolve themselves.")
 (defconstant +collision-slop+ 0.1
   "Amount of allowed penetration.  Used to reduce vibrating contacts.")
+
+(defstruct (contact (:constructor make-contact (point normal distance hash)))
+  ;; Contact point and normal
+  point normal
+  ;; Penetration distance
+  distance
+  ;; Calculated by arbiter-prestep
+  r1 r2
+  n-mass t-mass bounce
+  ;; Persistant contact information
+  (jn-acc 0) (jt-acc 0) (j-bias 0)
+  bias
+  ;; Hash value used as a (mostly) unique ID
+  hash)
