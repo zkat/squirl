dede9db8cd8f08b7d53c841076373339ae248523
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 16:27:17 -0600

    renaming poly-shape struct to just poly.

--- src/poly-shape.lisp
+++ src/poly-shape.lisp
@@ -1,16 +1,16 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
-(defstruct poly-shape-axis
+(defstruct poly-axis
   normal distance)
 
-(defstruct (poly-shape (:constructor %make-poly-shape (body vertices offset))
+(defstruct (poly (:constructor %make-poly (body vertices offset))
                        (:include shape))
   vertices axes transformed-vertices transformed-axes)
 
-(defun make-poly-shape (body vertices offset)
-  (let ((poly-shape (%make-poly-shape body verticel offset)))
-    poly-shape))
+(defun make-poly (body vertices offset)
+  (let ((poly (%make-poly body verticel offset)))
+    poly))
 
 (defun validate-vertices (vertices)
   "Check that a set of vertices has a correct winding, and that they form a convex polygon."
