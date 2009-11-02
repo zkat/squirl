ea2435b0a49f5bf6e148e843a577a1ea88f19238
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-02 03:28:52 +0200

    Added hashset types

--- src/hashset.lisp
+++ src/hashset.lisp
@@ -12,3 +12,15 @@
 (defun next-prime (n)
   (loop for prime in *primes* when (> prime n) return prime
      finally (error "Time to switch to native hashtables!")))
+
+(defstruct hashset-bin
+  elt hash next)
+
+(defstruct (hashset
+             (:constructor
+              make-hashset (size test transformer &aux
+                                 (size (next-prime size))
+                                 (bins (make-array size :element-type 'hashset-bin)))))
+  (entries 0) size
+  test transformer
+  (default-value nil) bins)
