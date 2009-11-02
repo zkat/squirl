195ee21a89b8c1ed9d5d0270e5f6e66f36688b97
Benjamin Saunders <besaunde@sfu.ca>
2009-11-01 22:30:08 -0800

    Ported circle-to-circle-query.

--- src/collision.lisp
+++ src/collision.lisp
@@ -2,3 +2,20 @@
 (in-package :squirl)
 
 (declaim (optimize safety debug))
+
+(defun circle-to-circle-query (p1 p2 r1 r2)
+  (let* ((delta (vec- p2 p1))
+         (mindist (+ r1 r2))
+         (distsq (vec-length-sq delta)))
+   (when (< distsq
+            (expt mindist 2))
+     (let* ((dist (sqrt distsq)))
+       (flet ((maybe/ (a b)
+                (if (= b 0) 0
+                    (/ a b))))
+        (make-contact (vec+ p1 (vec* delta
+                                     (+ 0.5 (maybe/ (- r1 (/ mindist 2))
+                                                    dist))))
+                      (vec* delta (maybe/ 1 dist))
+                      (- dist mindist)
+                      0))))))
