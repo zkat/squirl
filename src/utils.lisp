84a50a18018c311079a6634dbead1e215b5d8f46
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-02 09:37:18 +0200

    Changed util to DELETE-IFF

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,10 +5,10 @@
 
 ;; from alexandria:
 (declaim (inline delete/swapped-arguments))
-(defun delete/swapped-arguments (sequence item &rest keyword-arguments)
-  (apply #'delete item sequence keyword-arguments))
+(defun delete/swapped-arguments (sequence predicate &rest keyword-arguments)
+  (apply #'delete-if predicate sequence keyword-arguments))
 
-(define-modify-macro deletef (item &rest remove-keywords)
+(define-modify-macro delete-iff (predicate &rest remove-keywords)
   delete/swapped-arguments
   "Modify-macro for DELETE. Sets place designated by the first argument to
 the result of calling DELETE with ITEM, place, and the REMOVE-KEYWORDS.")
