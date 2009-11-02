3bcab72943db4396a2554c0403e076276a2c366c
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-02 09:40:59 +0200

    Fiddling more with the utils

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,10 +5,15 @@
 
 ;; from alexandria:
 (declaim (inline delete/swapped-arguments))
-(defun delete/swapped-arguments (sequence predicate &rest keyword-arguments)
-  (apply #'delete-if predicate sequence keyword-arguments))
+(defun delete/swapped-arguments (sequence delete-form &rest keyword-arguments)
+  (apply #'delete-if delete-form sequence keyword-arguments))
 
-(define-modify-macro delete-iff (predicate &rest remove-keywords)
+(define-modify-macro deletef (item &rest remove-keywords)
   delete/swapped-arguments
   "Modify-macro for DELETE. Sets place designated by the first argument to
 the result of calling DELETE with ITEM, place, and the REMOVE-KEYWORDS.")
+
+(define-modify-macro delete-iff (predicate &rest remove-keywords)
+  delete/swapped-arguments
+  "Modify-macro for DELETE-IF. Sets place designated by the first argument to
+the result of calling DELETE with PREDICATE, place, and the REMOVE-KEYWORDS.")
