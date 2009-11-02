454af3036ad478bd1d6d24627b099abd9ffa6a93
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-02 11:19:03 +0200

    Defining the WORLD-HASH type

--- src/world-hash.lisp
+++ src/world-hash.lisp
@@ -9,3 +9,13 @@
   retain                                ; Retain count
   ;; Used to prevent duplicate identification of an object within one query
   stamp)
+
+(defstruct world-hash
+  "The spatial hash is SquirL's default (and currently only) spatial index"
+  size                                  ; Number of cells in the table
+  cell-size                             ; Size of the cells
+  bbox-function                         ; Bounding box callback
+  handle-set                            ; `hash-set' of all handles
+  table                                 ; Bins in use
+  junk-bins                             ; The "recycle bin"
+  )
