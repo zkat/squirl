c6f7cc95be1e9f72602ef1825557db84c9c16d2f
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-03 06:27:49 +0200

    Documenting a little about bboxen

--- src/bounding-box.lisp
+++ src/bounding-box.lisp
@@ -4,21 +4,25 @@
 (declaim (optimize speed debug))
 
 (defstruct (bbox (:constructor make-bbox (left bottom right top)))
+  "Bounding box used to simplify collision detection"
   left bottom right top)
 
 (defun bbox-intersects-p (a b)
+  "Tests whether `bbox' A and B intersect"
   (and (<= (bbox-left a)   (bbox-right b))
        (<= (bbox-left b)   (bbox-right a))
        (<= (bbox-bottom a) (bbox-top b))
        (<= (bbox-bottom b) (bbox-top a))))
 
 (defun contains-bbox-p (bb other)
+  "Tests whether `bbox' BB entirely contains the OTHER one"
   (and (< (bbox-left bb)   (bbox-left other))
        (> (bbox-right bb)  (bbox-right other))
        (< (bbox-bottom bb) (bbox-bottom other))
        (> (bbox-top bb)    (bbox-top other))))
 
 (defun bbox-containts-vec-p (bb vec)
+  "Tests whether `vec' VEC is entirely contained by `bbox' BB"
   (with-vec vec
     (and (< (bbox-left bb)   vec.x)
          (> (bbox-right bb)  vec.x)
