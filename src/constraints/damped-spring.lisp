e4f25f0d29655e0b1b6a167b47557acfebfa531c
Kat Marchán <kzm@sykosomatic.org>
2009-11-03 10:13:29 -0600

    Wrote SPRING-FORCE (damped-spring) method.

--- src/constraints/damped-spring.lisp
+++ src/constraints/damped-spring.lisp
@@ -4,3 +4,7 @@
 (defstruct (damped-spring (:include spring))
   anchor1 anchor2 rest-length damping target-vrn r1 r2 n-mass n)
 
+(defmethod spring-force ((spring damped-spring) distance)
+  (* (- (damped-spring-rest-length spring) distance)
+     (spring-stiffness spring)))
+
