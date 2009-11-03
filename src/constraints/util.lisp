97903dca5b0a728a3ff6f04925e48d7ef7bc9fff
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 18:30:31 -0600

    Ported NORMAL-RELATIVE-VELOCITY

--- src/constraints/util.lisp
+++ src/constraints/util.lisp
@@ -6,3 +6,6 @@
         (vec* (vec-perp r1) (body-angular-velocity body1))
         (body-velocity body2)
         (vec* (vec-perp r2) (body-angular-velocity body2))))
+
+(defun normal-relative-velocity (body1 body2 r1 r2 normal)
+  (vec. (relative-velocity body1 body2 r1 r2) normal))
