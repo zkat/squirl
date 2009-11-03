60d5334b9bb1df0e4c5035ed5b198fd143d18f80
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 23:31:28 -0600

    Initial defstruct for damped-rotary-spring

--- src/constraints/damped-rotary-spring.lisp
+++ src/constraints/damped-rotary-spring.lisp
@@ -1,3 +1,6 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
+(defstruct (damped-rotary-spring (:include constraint))
+  rest-angle stiffness damping dt target-wrn i-sum)
+
