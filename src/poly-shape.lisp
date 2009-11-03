950feea5c1459b3c56b67e1df045ef45a26bdc86
Kat Marchán <kzm@sykosomatic.org>
2009-11-03 02:43:13 -0600

    "It's vectors all the way down, until you get to the turtles."

--- src/poly-shape.lisp
+++ src/poly-shape.lisp
@@ -28,13 +28,11 @@
 
 (defun set-up-vertices (poly vertices offset)
   (loop for vert in vertices for i from 0
-     for axis in (poly-axes poly)
      for a = (vec+ offset vert)
-     for b = (vec+ offset (elt vertices (mod (1+ i) (length vertices))))
+     for b = (vec+ offset (svref vertices (mod (1+ i) (length vertices))))
      for normal = (vec-normalize (vec-perp (vec- b a)))
-     do (setf (elt (poly-vertices poly) i) a
-              (poly-axis-normal axis) normal
-              (poly-axis-distance axis) (vec. normal a))))
+     do (setf (svref (poly-vertices poly) i) a
+              (svref (poly-axes poly) i) (make-poly-axis normal (vec. normal a)))))
 
 (defun make-poly (body vertices offset)
   (let ((poly (%make-poly body (length vertices))))
@@ -46,9 +44,9 @@
   "Check that a set of vertices has a correct winding, and that they form a convex polygon."
   (loop with length = (length vertices)
      for i from 0
-     for vert-a in vertices
-     for vert-b = (elt vertices (mod (1+ i) length))
-     for vert-c = (elt vertices (mod (+ i 2) length))
+     for vert-a across vertices
+     for vert-b = (svref vertices (mod (1+ i) length))
+     for vert-c = (svref vertices (mod (+ i 2) length))
      unless (minusp (vecx (vec- vert-b vert-a) (vec- vert-c vert-b)))
      return nil
      finally (return t)))
@@ -57,11 +55,11 @@
   (length (poly-vertices poly)))
 
 (defun nth-vertex (index poly)
-  (elt (poly-vertices poly) index))
+  (svref (poly-vertices poly) index))
 
 (defun poly-value-on-axis (poly normal distance)
   "Returns the minimum distance of the polygon to the axis."
-  (- (loop for vertex in (poly-vertices poly)
+  (- (loop for vertex across (poly-vertices poly)
         minimizing (vec. normal vertex))
      distance))
 
@@ -73,35 +71,30 @@
 
 (defun partial-poly-contains-vertex-p (poly vertex normal)
   "Same as POLY-CONTAINS-VERTEX-P, but ignores faces pointing away from NORMAL."
-  ;; More hilarity. I'm honestly not sure that this translation is correct.
-  (notany (fun (unless (vec. (poly-axis-normal _) normal)
-                 (plusp (- (vec. (poly-axis-normal _) vertex)
-                           (poly-axis-distance _)))))
-          (poly-transformed-axes poly)))
+  (loop for axis across (poly-transformed-axes poly)
+     never (unless (vec. (poly-axis-normal axis) normal)
+             (> (vec. (poly-axis-normal axis) vertex)
+                (poly-axis-distance axis)))))
 
 (defun poly-transform-vertices (poly position rotation)
-  (setf (poly-transformed-vertices poly)
-        ;; this'll have to become a MAP-INTO, I think.
-        (map 'list (fun (vec+ position (vec-rotate _ rotation)))
-             (poly-vertices poly))))
+  (map-into (poly-transformed-vertices poly)
+            (fun (vec+ position (vec-rotate _ rotation)))
+            (poly-vertices poly)))
 
 (defun poly-transform-axes (poly position rotation)
   (flet ((transformed-axis (axis)
            (let ((normal (vec-rotate (poly-axis-normal axis) rotation)))
-             (make-poly-axis
-              :normal normal
-              :distance (+ (vec. position normal) (poly-axis-distance axis))))))
-    (setf (poly-transformed-axes poly)
-          (map 'list #'transformed-axis (poly-axes poly)))))
+             (make-poly-axis normal (+ (vec. position normal) (poly-axis-distance axis))))))
+    (map-into (poly-transformed-axes poly) #'transformed-axis (poly-axes poly))))
 
 (defmethod shape-cache-data ((poly poly) position rotation)
   (poly-transform-vertices poly position rotation)
   (poly-transform-axes poly position rotation)
   (let* ((verts (poly-transformed-vertices poly))
-         (left (vec-x (elt verts 0)))
-         (right (vec-x (elt verts 0)))
-         (top (vec-y (elt verts 0)))
-         (bottom (vec-y (elt verts 0))))
+         (left (vec-x (svref verts 0)))
+         (right (vec-x (svref verts 0)))
+         (top (vec-y (svref verts 0)))
+         (bottom (vec-y (svref verts 0))))
     (loop for vert in verts
        do (setf left (min left (vec-x vert))
                 right (max right (vec-x vert))
@@ -118,21 +111,19 @@
   (declare (ignore layers group))
   (let ((axes (poly-axes poly))
         (vertices (poly-vertices poly)))
-    (loop for vert in vertices
-         for axis in axes
-         for i from 0
-         do
-         (let* ((normal (poly-axis-normal axis))
-                (a-normal (vec. a normal)))
-           (unless (> (poly-axis-distance axis) a-normal)
-             (let* ((b-normal (vec. b normal))
-                    (ratio (/ (- (poly-axis-distance axis) a-normal)
-                              (- b-normal a-normal))))
-               (unless (or (< ratio 0) (< 1 ratio))
-                 (let* ((point (vec-lerp a b ratio))
-                        (dt (- (vecx normal point)))
-                        (dt-min (- (vecx normal vert)))
-                        (dt-max (- (vecx normal (elt vertices (mod (1+ i) (length vertices)))))))
-                   (when (<= dt-min dt dt-max)
-                     (values poly ratio normal))))))))))
-
+    (loop for vert across vertices
+       for axis across axes
+       for i from 0
+       do (let* ((normal (poly-axis-normal axis))
+                 (a-normal (vec. a normal)))
+            (unless (> (poly-axis-distance axis) a-normal)
+              (let* ((b-normal (vec. b normal))
+                     (ratio (/ (- (poly-axis-distance axis) a-normal)
+                               (- b-normal a-normal))))
+                (when (<= 0 ratio 1)
+                  (let* ((point (vec-lerp a b ratio))
+                         (dt (- (vecx normal point)))
+                         (dt-min (- (vecx normal vert)))
+                         (dt-max (- (vecx normal (svref vertices (mod (1+ i) (length vertices)))))))
+                    (when (<= dt-min dt dt-max)
+                      (values poly ratio normal))))))))))
