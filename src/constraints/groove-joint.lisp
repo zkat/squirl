89a491914f6ac4aa6e485ddcedca750142e73950
Kat Marchán <kzm@sykosomatic.org>
2009-11-03 11:13:45 -0600

    Wrote groove-joint constructor.

--- src/constraints/groove-joint.lisp
+++ src/constraints/groove-joint.lisp
@@ -1,3 +1,14 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
+(defstruct (groove-joint (:include constraint)
+                         (:constructor
+                          make-groove-joint
+                          (body-a body-b groove-a groove-b anchor2
+                                  &aux (groove-normal
+                                        (vec-perp (vec-normalize (vec- groove-b groove-a)))))))
+  groove-normal groove-a groove-b anchor2
+  groove-transformed-normal clamp
+  r1 r2 k1 k2 (j-acc +zero-vector+) j-max-length bias)
+
+
