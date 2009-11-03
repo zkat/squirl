2372ed7058dd541f114169bf0b24833c27e41921
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 18:32:09 -0600

    Ported K-TENSOR. Holy shit that sucked.

--- src/constraints/util.lisp
+++ src/constraints/util.lisp
@@ -30,3 +30,44 @@
           (body-inverse-inertia body1))
        (* r2-cross-normal r2-cross-normal
           (body-inverse-inertia body2)))))
+
+(defun k-tensor (body1 body2 r1 r2)
+  ;; calculate mass matrix
+  ;; C sources say: "If I wasn't lazy and wrote a proper matrix class, this wouldn't be so gross...
+  (let* ((b1-inverse-mass (body-inverse-mass body1))
+         (b2-inverse-mass (body-inverse-mass body2))
+         (mass-sum (+ b1-inverse-mass b2-inverse-mass))
+
+         ;; Start with I*mass-sum
+         (k11 mass-sum) (k12 0)
+         (k21 0)(k22 mass-sum)
+
+         ;; influence from r1
+         (b1-inverse-inertia (body-inverse-inertia body1))
+         (r1xsq (* (vec-x r1) (vec-x r1) b1-inverse-inertia))
+         (r1ysq (* (vec-y r1) (vec-y r1) b1-inverse-inertia))
+         (r1nxy (- (* (vec-x r1) (vec-y r1) b1-inverse-inertia)))
+
+         ;; influence from r2
+         (b2-inverse-inertia (body-inverse-inertia body2))
+         (r2xsq (* (vec-x r2) (vec-x r2) b2-inverse-inertia))
+         (r2ysq (* (vec-y r2) (vec-y r2) b2-inverse-inertia))
+         (r2nxy (- (* (vec-x r2) (vec-y r2) b2-inverse-inertia))))
+    ;; apply influence from r1
+    (incf k11 r1ysq)
+    (incf k12 r1nxy)
+    (incf k21 r1nxy)
+    (incf k22 r1xsq)
+
+    ;; apply influence from r2
+    (incf k11 r2ysq)
+    (incf k12 r2nxy)
+    (incf k21 r2nxy)
+    (incf k22 r2xsq)
+
+    ;; invert
+    (let ((det-inv (/ (- (* k11 k22) (* k12 k21)))))
+
+      ;; and we're done.
+      (values (vec (* k22 det-inv) (- (* k12 det-inv)))
+              (vec (- (* k21 det-inv)) (* k11 det-inv))))))
