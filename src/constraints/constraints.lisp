3da9a7389c3aa0ccf19d5784180a40e4680391ba
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 18:57:05 -0600

    Constraint defstruct.

--- src/constraints/constraints.lisp
+++ src/constraints/constraints.lisp
@@ -6,3 +6,11 @@
 (defgeneric pre-step (constraint dt dt-inverse))
 (defgeneric apply-impulse (constraint))
 (defgeneric get-impulse (constraint))
+
+(defstruct (constraint (:constructor (make-constraint (body-a body-b))))
+  body-a body-b
+  (max-force most-positive-double-float)
+  (bias-coefficient *constraint-bias-coefficient*)
+  (max-bias most-positive-double-float)
+  data)
+
