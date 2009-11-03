b1ce074a3647f8a2085a8a103d4f1eaa6a1646f9
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 18:30:12 -0600

    Ported RELATIVE-VELOCITY

--- src/constraints/util.lisp
+++ src/constraints/util.lisp
@@ -1,3 +1,8 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
+(defun relative-velocity (body1 body2 r1 r2)
+  (vec+ (body-velocity body1)
+        (vec* (vec-perp r1) (body-angular-velocity body1))
+        (body-velocity body2)
+        (vec* (vec-perp r2) (body-angular-velocity body2))))
