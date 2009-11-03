5a631f24e6a6f517cbb6c88c6649f3ba63fc49bc
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 18:30:49 -0600

    Ported APPLY-IMPULSES

--- src/constraints/util.lisp
+++ src/constraints/util.lisp
@@ -9,3 +9,8 @@
 
 (defun normal-relative-velocity (body1 body2 r1 r2 normal)
   (vec. (relative-velocity body1 body2 r1 r2) normal))
+
+(defun apply-impulses (body1 body2 r1 r2 j)
+  (body-apply-impulse body1 (vec-neg j) r1)
+  (body-apply-impulse body2 j r2)
+  (values))
