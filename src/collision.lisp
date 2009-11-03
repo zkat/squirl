9d827aedd71745ad989f2c2d738fa4249ba49db7
Benjamin Saunders <besaunde@sfu.ca>
2009-11-03 14:14:04 -0800

    Ported find-vertices.

--- src/collision.lisp
+++ src/collision.lisp
@@ -77,8 +77,18 @@
 
 (defun find-vertices (poly1 poly2 normal distance)
   "Add contacts for penetrating vertices"
-  ;; todo
-  )
+  (let ((contacts))
+    (loop
+      for i from 0
+      for vertex in (poly-vertices poly1)
+      do (when (partial-poly-contains-vertex-p poly2 vertex (vec-neg normal))
+           (push (make-contact vertex normal distance (hash-pair poly1 i)) contacts)))
+    (loop
+      for i from 0
+      for vertex in (poly-vertices poly2)
+      do (when (partial-poly-contains-vertex-p poly1 vertex (vec-neg normal))
+           (push (make-contact vertex normal distance (hash-pair poly2 i)) contacts))
+       finally (return contacts))))
 
 (defun segment-value-on-axis (segment normal distance)
   (- (min (- (vec. normal (segment-trans-a segment)) (segment-radius segment))
@@ -90,7 +100,7 @@
   ;; todo
   )
 
-(defun segment-to-poly (segment poly)
+t(defun segment-to-poly (segment poly)
   ;; todo
   )
 (defun circle-to-poly (circle poly)
