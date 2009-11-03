c3f9ad649cefa0256728829ab53ab447d058c89d
Kat Marchán <kzm@sykosomatic.org>
2009-11-03 10:12:05 -0600

    Naive implementation of damped-spring struct.

--- src/constraints/damped-spring.lisp
+++ src/constraints/damped-spring.lisp
@@ -1,3 +1,6 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
+(defstruct (damped-spring (:include spring))
+  anchor1 anchor2 rest-length damping target-vrn r1 r2 n-mass n)
+
