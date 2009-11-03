abec27fe74635c8a1ea47c5b8b1dc52892a93400
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 23:37:44 -0600

    Wrote PRE-STEP (damped-rotary-spring) method.

--- src/constraints/damped-rotary-spring.lisp
+++ src/constraints/damped-rotary-spring.lisp
@@ -4,3 +4,16 @@
 (defstruct (damped-rotary-spring (:include constraint))
   rest-angle stiffness damping dt target-wrn i-sum)
 
+(defmethod pre-step ((spring damped-rotary-spring) dt dt-inverse)
+  (let ((body-a (constraint-body-a spring))
+        (body-b (constraint-body-b spring)))
+    (setf (damped-rotary-spring-i-sum spring) (/ (+ (body-inverse-inertia body-a)
+                                                    (body-inverse-inertia body-b)))
+          (damped-rotary-spring-dt spring) dt
+          (damped-rotary-spring-target-wrn spring) 0.0)
+
+    ;; Applying spring torque
+    (let ((j-spring (* dt (spring-torque spring (- (body-angle body-a) (body-angle body-b))))))
+      (decf (body-angular-velocity body-a) (* j-spring (body-inverse-inertia body-a)))
+      (incf (body-angular-velocity body-b) (* j-spring (body-inverse-inertia body-b)))))
+  (values))
