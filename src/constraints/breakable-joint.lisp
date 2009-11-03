85352979d0fae9d3e90845f92cb028114a28eede
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 23:20:30 -0600

    Wrote PRE-STEP (breakable-joint) method.

--- src/constraints/breakable-joint.lisp
+++ src/constraints/breakable-joint.lisp
@@ -3,3 +3,13 @@
 
 (defstruct breakable-joint
   delegate space last-dt-inverse)
+
+(defmethod pre-step ((joint breakable-joint) dt dt-inverse)
+  (let ((delegate (breakable-joint-delegate joint)))
+    (if (>= (* (get-impulse delegate) (breakable-joint-last-dt-inverse joint))
+            (breakable-joint-max-force joint))
+        ;; remove the breakable joint from the space...
+        (space-remove-constraint (breakable-joint-space joint) joint)
+        ;; otherwise, call pre-step on its delegate.
+        (prog1 (pre-step delegate dt dt-inverse)
+          (setf (breakable-joint-last-dt-inverse joint) dt-inverse)))))
