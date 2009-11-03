bc64604504a8b438d2bfcfd1805af2270d1ddf16
Kat Marchán <kzm@sykosomatic.org>
2009-11-02 19:32:11 -0600

    Wrote PRE-STEP (pin-joint) method.

--- src/constraints/pin-joint.lisp
+++ src/constraints/pin-joint.lisp
@@ -20,3 +20,32 @@
   jn-max
   bias)
 
+(defmethod pre-step ((joint pin-joint) dt dt-inverse)
+  (let ((body-a (pin-joint-body-a joint))
+        (body-b (pin-joint-body-b joint)))
+    (setf (pin-joint-r1 joint) (vec-rotate (pin-joint-anchor1 joint)
+                                           (body-rotation body-a))
+          (pin-joint-r2 joint) (vec-rotate (pin-joint-anchor2 joint)
+                                           (body-rotation body-b)))
+    (let* ((delta (vec- (vec+ (body-position body-b)
+                              (pin-joint-r2 joint))
+                        (vec+ (body-position body-a)
+                              (pin-joint-r2 joint))))
+           (distance (vec-length delta))
+           (max-bias (pin-joint-max-bias joint)))
+      (setf (pin-joint-normal joint) (vec* delta (/ (if (zerop distance)
+                                                        most-positive-double-float
+                                                        distance)))
+            (pin-joint-n-mass joint) (/ (k-scalar body-a body-b
+                                                  (pin-joint-r1 joint)
+                                                  (pin-joint-r2 joint)
+                                                  (pin-joint-normal joint)))
+            (pin-joint-bias joint) (clamp (- (* (pin-joint-bias-coefficient joint)
+                                                dt-inverse
+                                                (- distance (pin-joint-distance joint))))
+                                          (- max-bias) max-bias)
+            (pin-joint-jn-max joint) (impulse-max joint dt))
+      (apply-impulses body-a body-b (pin-joint-r1 joint) (pin-joint-r2 joint)
+                      (vec* (pin-joint-normal joint) (pin-joint-jn-acc joint)))))
+  (values))
+
