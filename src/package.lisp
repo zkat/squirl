0a5797ba1e5112a230e5c4096575e7b533b5e944
Kat Marchán <kzm@sykosomatic.org>
2009-11-05 13:46:28 -0600

    Exporting symbols.

--- src/package.lisp
+++ src/package.lisp
@@ -3,4 +3,52 @@
 (in-package :cl-user)
 
 (defpackage squirl
-  (:use :cl))
+  (:use :cl)
+  (:export
+
+   ;; Vector math
+   :vec
+   :vec-x
+   :vec-y
+   :+zero-vector+
+
+   ;; Bodies
+   :body
+   :make-body
+   :body-position
+
+   ;; Shapes
+   :circle
+   :make-circle
+   :segment
+   :make-segment
+   :poly
+   :make-poly
+
+   ;; moments
+   :moment-for-circle
+   :moment-for-segment
+   :moment-for-poly
+
+   ;; world
+   :world
+   :make-world
+   :map-world
+   :world-bodies
+   :world-constraints
+   :world-add-body
+   :world-add-shape
+   :world-add-static-shape
+   :world-add-constraint
+   :world-remove-shape
+   :world-remove-static-shape
+   :world-remove-body
+   :world-remove-constraint
+   :resize-world-static-hash
+   :resize-world-active-hash
+   :world-step
+
+   ;; callbacks
+   :collide
+   ))
+
