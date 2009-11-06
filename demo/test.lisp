bf7103659a37470fa979538a0d807e0ab141b918
Kat Marchán <kzm@sykosomatic.org>
2009-11-06 14:54:14 -0600

    Updated test.lisp

--- demo/test.lisp
+++ demo/test.lisp
@@ -16,30 +16,41 @@
          (y (vec-y position)))
     (draw-circle (make-point x y) 15 :resolution 30)))
 
-(defparameter *point-a* (make-point 0 10))
-(defparameter *point-b* (make-point 500 10))
-
 (defun draw-body (body)
   (let* ((position (body-position body))
          (x (vec-x position))
          (y (vec-y position)))
-    (draw-circle (make-point x y) 5 :resolution 30)))
+    (with-color *green*
+     (map nil #'draw-shape (body-shapes body)))
+    (draw-circle (make-point x y) 2 :resolution 30 :color *red*)))
 
 (defreply init ((demo =squirl-demo=))
   (setf (world demo) (make-world :gravity (vec 0 -100)))
-  (let ((body (make-body most-positive-double-float most-positive-double-float 0 0)))
-    (world-add-static-shape (world demo) (make-segment body
-                                                       (vec (point-x *point-a*)
-                                                            (point-y *point-a*))
-                                                       (vec (point-x *point-b*)
-                                                            (point-y *point-b*)) 3))
+  (let ((body (make-body))
+        (floor (make-segment (vec 0 10) (vec 500 10))))
+    (attach-shape floor body)
     (world-add-body (world demo) body)))
 
+(defgeneric draw-shape (shape)
+  (:method ((circle circle))
+    (let ((circle-center (circle-transformed-center circle)))
+      (draw-circle (make-point (vec-x circle-center) (vec-y circle-center))
+                   (round (circle-radius circle)) :filledp nil)))
+  (:method ((seg segment))
+    (let ((a (segment-trans-a seg))
+          (b (segment-trans-b seg)))
+      (draw-line (make-point (vec-x a) (vec-y a))
+                 (make-point (vec-x b) (vec-y b)))))
+  (:method ((poly poly))
+    (let ((vertices (poly-transformed-vertices poly)))
+      (loop for i below (length vertices)
+         for a = (elt vertices i)
+         for b = (elt vertices (mod (1+ i) (length vertices)))
+         do (draw-line (make-point (vec-x a) (vec-y a))
+                       (make-point (vec-x b) (vec-y b)))))))
+
 (defreply draw ((demo =squirl-demo=) &key)
-  (draw-line *point-a* *point-b* :color *red*)
-  (map nil #'draw-a-circle (world-bodies (world demo)))
-  (with-color *green*
-    (map nil #'draw-body (world-bodies (world demo)))))
+  (map nil #'draw-body (world-bodies (world demo))))
 
 ;; This allows us to fix the physics timestep without fixing the framerate.
 ;; This means the physics -should- run at the same perceived speed no matter
@@ -49,19 +60,17 @@
   (incf (accumulator demo) (if (> dt 0.5) 0.5 dt))
   (loop while (>= (accumulator demo) (physics-timestep demo))
      do (world-step (world demo) (physics-timestep demo))
-     (decf (accumulator demo) (physics-timestep demo)))
-  ;; remove shapes that are long gone
-  (map nil (lambda (c)
-             (world-remove-body (world demo) c)
-             (map nil (lambda (x) (world-remove-shape (world demo) x)) (squirl::body-shapes c)))
+       (decf (accumulator demo) (physics-timestep demo)))
+  ;; remove bodies that are long gone
+  (map nil (lambda (c) (world-remove-body (world demo) c))
        (remove-if (lambda (c) (> (vec-y (body-position c)) -100)) (world-bodies (world demo)))))
 
 (defun add-circle (demo x y)
   (let* ((mass 1)
          (radius 15)
          (inertia (moment-for-circle mass 0 radius (vec 0 0)))
-         (body (make-body mass inertia x y)))
-    (world-add-shape (world demo) (make-circle body radius (vec 0 0)))
+         (body (make-body :mass mass :inertia inertia :position (vec x y))))
+    (attach-shape (make-circle radius) body)
     (world-add-body (world demo) body)))
 
 (defreply mouse-down ((engine =squirl-demo=) button)
