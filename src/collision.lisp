61e96aeb48654c04f4cd89c5896febbacec79f1c
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-06 19:24:18 +0200

    Using DO-VECTOR in FIND-VERTICES

--- src/collision.lisp
+++ src/collision.lisp
@@ -69,17 +69,12 @@
 
 (defun find-vertices (poly1 poly2 normal distance &aux contacts)
   "Add contacts for penetrating vertices"
-  (loop
-     for i from 0
-     for vertex across (poly-vertices poly1)
-     when (partial-poly-contains-vertex-p poly2 vertex (vec-neg normal))
-     do (push (make-contact vertex normal distance (hash-pair (shape-id poly1) i)) contacts))
-  (loop
-     for i from 0
-     for vertex across (poly-vertices poly2)
-     when (partial-poly-contains-vertex-p poly1 vertex (vec-neg normal))
-     do (push (make-contact vertex normal distance (hash-pair (shape-id poly2) i)) contacts))
-  contacts)
+  (do-vector ((i vertex) (poly-vertices poly1))
+    (when (partial-poly-contains-vertex-p poly2 vertex (vec-neg normal))
+      (push (make-contact vertex normal distance (hash-pair (shape-id poly1) i)) contacts)))
+  (do-vector ((i vertex) (poly-vertices poly2) contacts)
+    (when (partial-poly-contains-vertex-p poly1 vertex (vec-neg normal))
+      (push (make-contact vertex normal distance (hash-pair (shape-id poly2) i)) contacts))))
 
 (defun segment-value-on-axis (segment normal distance)
   (- (min (- (vec. normal (segment-trans-a segment)) (segment-radius segment))
