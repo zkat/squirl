23a89abe4de9ccef67dfb301eddd52af7aca69d6
Benjamin Saunders <besaunde@sfu.ca>
2009-11-10 20:15:54 -0800

    First-attempt port of the chipmunk Pyramid demo.

---
+++ demo/pyramid.lisp
@@ -0,0 +1,123 @@
+(defpackage #:squirl-demo-pyramid
+  (:use :cl :uid :squirl :sheeple)
+  (:export :run-demo))
+(in-package :squirl-demo-pyramid)
+
+(defvar *world*)
+(defvar *floor*)
+
+(defvar *elapsed* 0)
+
+(defparameter *step* (/ 1 60 2))
+
+(defproto =squirl-demo-pyramid= (=engine=)
+  ((window-width 640)
+   (window-height 480)))
+
+(defreply update ((demo =squirl-demo-pyramid=) delta &key)
+  (incf *elapsed* delta)
+  (loop while (>= *elapsed* *step*) do
+       (world-step *world* *step*)
+       (decf *elapsed* *step*)))
+
+(defreply init :after ((demo =squirl-demo-pyramid=))
+  (reset-shape-id-counter)
+  (setf *world* (make-world :iterations 20
+                            :gravity (vec 0 -300)))
+  (resize-world-active-hash *world* 40.0 2999)
+  (resize-world-static-hash *world* 40.0 999)
+  
+  (setf *floor* (make-body))
+  (attach-shape (make-segment (vec -600 -240)
+                              (vec 600 -240)
+                              :radius 0d0
+                              :elasticity 1
+                              :friction 1)
+                  *floor*)
+  (world-add-body *world* *floor*)
+  
+  (let ((friction 0.6)
+        (verts (list (vec -3 -20)
+                     (vec -3 20)
+                     (vec 3 20)
+                     (vec 3 -20))))
+    (loop
+       with n = 9
+       for i from 0 to n
+       for offset = (vec (- (/ (* i 60) 2))
+                         (* (- n i) 52))
+       do (loop
+             for j from 0 to i do
+               (mapc (lambda (body)
+                       (attach-shape (make-poly verts
+                                                :friction friction
+                                                :elasticity 0)
+                                     body)
+                       (world-add-body *world* body))
+                     (nconc
+                      (list (make-body
+                             :mass 1
+                             :inertia (moment-for-poly 1 verts)
+                             :position (vec+ (vec (* j 60) -220)
+                                             offset))
+                            (make-body
+                             :mass 1
+                             :inertia (moment-for-poly 1 verts)
+                             :position (vec+ (vec (* j 60) -197)
+                                             offset)
+                             :angle (/ pi 2)))
+                      (unless (= j (1- i))
+                        (list (make-body
+                               :mass 1
+                               :inertia (moment-for-poly 1 verts)
+                               :position (vec+ (vec (+ (* j 60) 30)
+                                                    -191)
+                                               offset)
+                               :angle (/ pi 2)))))))
+         (mapc (lambda (body)
+                 (attach-shape (make-poly verts
+                                          :friction friction
+                                          :elasticity 0)
+                               body)
+                 (world-add-body *world* body))
+               (list (make-body
+                      :mass 1
+                      :inertia (moment-for-poly 1 verts)
+                      :position (vec+ (vec (+ (* (1- i) 60) 17)
+                                           -174)
+                                      offset)))))))
+
+(defreply draw ((demo =squirl-demo-pyramid=))
+  (map-world #'draw-body *world*))
+
+(defun draw-body (body)
+  (let* ((position (body-position body))
+         (x (vec-x position))
+         (y (vec-y position)))
+    (with-color *green*
+      (map nil #'draw-shape (body-shapes body)))
+    (draw-circle (make-point x y) 2 :resolution 30 :color *red*)))
+
+(defgeneric draw-shape (shape)
+  (:method ((circle circle))
+    (let* ((circle-center (circle-transformed-center circle))
+           (edge (vec* (body-rotation (shape-body circle))
+                       (circle-radius circle)))
+           (edge-t (vec+ edge circle-center))
+           (edge-neg-t (vec- circle-center edge)))
+      (draw-circle (make-point (vec-x circle-center) (vec-y circle-center))
+                   (round (circle-radius circle)) :filledp nil)
+      (draw-line (make-point (vec-x edge-t) (vec-y edge-t))
+                 (make-point (vec-x edge-neg-t) (vec-y edge-neg-t)))))
+  (:method ((seg segment))
+    (let ((a (segment-trans-a seg))
+          (b (segment-trans-b seg)))
+      (draw-line (make-point (vec-x a) (vec-y a))
+                 (make-point (vec-x b) (vec-y b)))))
+  (:method ((poly poly))
+    (let ((vertices (poly-transformed-vertices poly)))
+      (loop for i below (length vertices)
+         for a = (elt vertices i)
+         for b = (elt vertices (mod (1+ i) (length vertices)))
+         do (draw-line (make-point (vec-x a) (vec-y a))
+                       (make-point (vec-x b) (vec-y b)))))))
