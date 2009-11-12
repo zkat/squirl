1df02c93dfd55d554f83e9b241537829e95955ca
Kat Marchán <kzm@sykosomatic.org>
2009-11-12 02:05:26 -0600

    Fixed drawing for pyramid.lisp; wrote and exported run-demo.

--- demo/pyramid.lisp
+++ demo/pyramid.lisp
@@ -25,7 +25,7 @@
     (gl:matrix-mode :projection)
     (gl:load-identity)
     (gl:viewport 0 0 window-width window-height)
-    (gl:ortho (- (/ window-width 2)) (/ window-width 2) (- (/ window-height 2)) (/ window-height 2) 10 0)
+    (gl:ortho (- window-width) window-width (- window-height) window-height 10 0)
     (gl:matrix-mode :modelview)))
 
 (defreply init :after ((demo =squirl-demo-pyramid=))
@@ -128,3 +128,6 @@
          for b = (elt vertices (mod (1+ i) (length vertices)))
          do (draw-line (make-point (vec-x a) (vec-y a))
                        (make-point (vec-x b) (vec-y b)))))))
+
+(defun run-demo ()
+  (run =squirl-demo-pyramid=))
