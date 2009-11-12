f5807c2fe7a85be68522d8080db0c9e65ffdb668
Adlai Chandrasekhar <munchking@gmail.com>
2009-11-12 10:36:49 +0200

    Revert "It's ugly but it's fast"
    This reverts commit f1070883aac6216896008aa8debf73924fc03684.

--- src/world-hash.lisp
+++ src/world-hash.lisp
@@ -55,11 +55,8 @@ list structure into the `world-hash-junk'."
 
 (defun clear-world-hash (hash)
   "Clear all cells in the `world-hash' HASH"
-  (declare (optimize speed (safety 0)))
-  (let ((table (world-hash-table hash)))
-    (declare (simple-vector table))
-    (dotimes (index (the fixnum (length table)))
-      (setf (svref table index) nil))))
+  (dotimes (index (world-hash-size hash))
+    (clear-hash-cell hash index)))
 
 (defun resize-world-hash (hash new-cell-size new-size)
   "Resize `world-hash' HASH to the specified dimensions"
