c139eeab551f3996642f6e4e1f564ef3456009c9
Kat Marchán <kzm@sykosomatic.org>
2009-11-17 04:32:06 -0600

    Critical control bugfix.

--- demo/squirl-demo.lisp
+++ demo/squirl-demo.lisp
@@ -133,10 +133,10 @@ makes sure that the current world is updated by 1 time unit per second."
   (let ((x -300) (y 220))
     (draw-string x y (format nil
                              "Controls:~@
-                              N chooses the next demo~@
+                              #\\N chooses the next demo~@
                               Use the mouse to grab objects~@
                               Arrow keys control some demos~@
-                              \\ toggles anti-aliasing."))))
+                              #\\A toggles anti-aliasing."))))
 
 (defun draw-fps ()
   (let ((x -300) (y 100))
@@ -188,7 +188,7 @@ makes sure that the current world is updated by 1 time unit per second."
     (#\n (run-demo (elt *demos*
                         (mod (1+ (position (class-name (class-of *current-demo*)) *demos*))
                              (length *demos*)))))
-    (#\\ (toggle-anti-aliasing))))
+    (#\a (toggle-anti-aliasing))))
 
 (defun toggle-anti-aliasing ()
   (if *aa-enabled-p*
