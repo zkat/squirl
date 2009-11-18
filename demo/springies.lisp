b240febd226c9ae86b4fcbe86e2c215363706095
Kat Marchán <kzm@sykosomatic.org>
2009-11-18 14:48:29 -0600

    Brilliant new strategy for grouping collisions.

--- demo/springies.lisp
+++ demo/springies.lisp
@@ -13,19 +13,44 @@
   (* (squirl::clamp (- (squirl::damped-spring-rest-length spring) distance (- clamp) clamp))
      (squirl::damped-spring-stiffness spring)))
 
-(defun add-bar (point-a point-b)
+(defclass springy ()
+  ((bodies :initarg :bodies :initform nil :accessor springy-bodies)))
+
+(defmethod initialize-instance :after ((springy springy) &key)
+  (loop for body in (springy-bodies springy) do (setf (body-actor body) springy)))
+
+(defcollision ((a springy) (b springy) contacts) (unless (eq a b) t))
+
+(defun build-springies (world)
+  (list (make-instance 'springy :bodies (list (add-bar world (vec -240 160) (vec -160 80))
+                                              (add-bar world (vec -160 80) (vec -80 160))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 0 160) (vec 80 0))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 160 160) (vec 240 160))))
+        (make-instance 'springy :bodies (list (add-bar world (vec -240 0) (vec -160 -80))
+                                              (add-bar world (vec -160 -80) (vec -80 0))
+                                              (add-bar world (vec -80 0) (vec 0 0))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 0 -80) (vec 80 -80))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 240 80) (vec 160 0))
+                                              (add-bar world (vec 160 0) (vec 240 -80))))
+        (make-instance 'springy :bodies (list (add-bar world (vec -240 -80) (vec -160 -160))
+                                              (add-bar world (vec -160 -160) (vec -80 -160))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 0 -160) (vec 80 -160))))
+        (make-instance 'springy :bodies (list (add-bar world (vec 160 -160) (vec 240 -160))))))
+
+(defun add-bar (world point-a point-b)
+  (let* ((center (vec* (vec+ point-a point-b) 1/2))
+         (length (vec-length (vec- point-a point-b)))
+         (mass (/ length 160)))
+    (world-add-body world
+                    (make-body :mass mass :inertia (* mass length (/ length 12)) :position center
+                               :shapes (list (make-segment (vec- point-a center) (vec- point-b center)
+                                                           :radius 10))))))
+
+(defun constrain-springies (world springies)
   ;; todo
   )
 
-(defun gen-bars (world)
-  ;; todo
-  )
-
-(defun constrain-bars (world bars)
-  ;; todo
-  )
-
-(defun add-springs (world bars)
+(defun add-springs (world springies)
   ;; todo
   )
 
@@ -34,9 +59,9 @@
         (world demo (make-world)))
   (resize-world-active-hash (world demo) 30 999)
   (resize-world-static-hash (world demo) 200 99)
-  (let ((bars (gen-bars (world demo))))
-    (constrain-bars (world demo) bars)
-    (add-springs (world demo) bars))
+  (let ((springies (build-springies (world demo))))
+    (constrain-springies (world demo) springies)
+    (add-springs (world demo) springies))
   (world demo))
 
 (pushnew 'springies-demo *demos*)
