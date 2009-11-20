710f10670d8edf621f52ce72cfad1ebd716a566a
Kat Marchán <kzm@sykosomatic.org>
2009-11-19 23:58:11 -0600

    Holy shit I love defcollision

--- demo/pump.lisp
+++ demo/pump.lisp
@@ -22,9 +22,25 @@
             (setf (body-velocity ball) +zero-vector+)
             (setf (body-position ball) (vec -224 200))))))
 
+(defclass ball () ())
+(defclass feeder () ())
+(defclass plunger () ())
+(defclass gear () ())
+
+;; balls collide with everything
+(defcollision ((ball ball) anything contacts) t)
+;; the plunger collides with the hopper, but not with the gears
+(defcollision ((a plunger) (hopper (eql :not-grabbable)) contacts) t)
+(defcollision ((a plunger) (b gear) contacts) nil)
+;; the feeder collides with the balls, but not with anything else
+(defcollision ((a feeder) anything contacts) nil)
+(defcollision ((feeder feeder) (ball ball) contacts) t)
+;; gears collide with each other, though
+(defcollision ((a gear) (another gear) contacts) t)
+
 (defun add-ball (world pos)
-  (world-add-body world (make-body :mass 1 :inertia (moment-of-inertia-for-circle 1 30 0) :position pos
-                                   :shapes (list (make-circle 30 :friction 0.5)) :actor 1)))
+  (world-add-body world (make-body :mass 1 :position pos :actor (make-instance 'ball)
+                                   :shapes (list (make-circle 30 :friction 0.5)))))
 
 (defun setup-static-body (world)
   (world-add-body world
@@ -49,21 +65,20 @@
                      (vec 30 64)
                      (vec 30 -80))))
     (world-add-body
-     world (make-body :mass 1 :position (vec -160 -80) :actor 1
+     world (make-body :mass 1 :position (vec -160 -80) :actor (make-instance 'plunger)
                       :shapes (list (make-poly verts :restitution 1 :friction 0.5))))))
 
 (defun add-small-gear (world static-body)
   (let ((gear (world-add-body
-               world (make-body :mass 10 :angle (/ pi -2)
-                                :position (vec -160 -160) :shapes (list (make-circle 80))
-                                :actor 0))))
+               world (make-body :mass 10 :angle (/ pi -2) :actor (make-instance 'gear)
+                                :position (vec -160 -160) :shapes (list (make-circle 80))))))
     (world-add-constraint world (make-pivot-joint static-body gear (vec -160 -160) +zero-vector+))
     gear))
 
 (defun add-big-gear (world static-body)
   (let ((gear (world-add-body
-               world (make-body :mass 40 :inertia (moment-of-inertia-for-circle 40 160 0)
-                                :position (vec 80 -160) :angle (/ pi 2) :actor 0
+               world (make-body :mass 40 :actor (make-instance 'gear)
+                                :position (vec 80 -160) :angle (/ pi 2)
                                 :shapes (list (make-circle 160))))))
     (world-add-constraint world (make-pivot-joint static-body gear (vec 80 -160) +zero-vector+))
     gear))
@@ -74,7 +89,7 @@
 
 (defun add-feeder (world static-body small-gear)
   (let* ((bottom -300) (top 32) (length (- top bottom))
-         (feeder (make-body :mass 1 :actor 1
+         (feeder (make-body :mass 1 :actor (make-instance 'feeder)
                             :position (vec -224 (/ (+ bottom top) 2))
                             :shapes (list (make-segment (vec 0 (/ length 2)) (vec 0 (- (/ length 2)))
                                                         :radius 20)))))
@@ -101,10 +116,5 @@
     (add-feeder (world demo) (demo-static-body demo) small-gear)
     (motorize-gear (world demo) (demo-static-body demo) big-gear))
   (world demo))
-
-(defcollision ((a number) (b symbol) collisions)
-  (declare (ignore collisions))
-  (unless (and (= a 0) (eq b :not-grabbable)) t))
-(defcollision ((a number) (b number) constraints) (= a b))
 
 (pushnew 'pump-demo *demos*)
