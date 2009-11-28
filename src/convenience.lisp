7aae03219a56960093124d83223b54243c9e0e3c
Kat Marchán <kzm@sykosomatic.org>
2009-11-28 00:18:25 -0600

    I'm a retard.

--- src/convenience.lisp
+++ src/convenience.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 (in-package :squirl)
 
-(defun make-rectangle (width height &key (restitution 0d0) (friction 0d0) (offset +zero-vector))
+(defun make-rectangle (width height &key (restitution 0d0) (friction 0d0) (offset +zero-vector+))
   (let* ((width/2 (/ width 2))
          (height/2 (/ height 2))
          (verts (list (vec (- width/2) height/2)
